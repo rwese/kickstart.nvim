05954e65f07c4e9908aae823f684184fcb186bf0
René Weselowski <rene.weselowski@gmail.com>
2024-03-27 05:01:05 +0100

    fix oil, tmux-navigator behavior

--- init.lua
+++ init.lua
@@ -658,5 +658,7 @@ cmp.setup {
   },
 }
 
+require("vim-options")
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,6 +1,25 @@
 return {
 	'stevearc/oil.nvim',
-	opts = {},
+	opts = {
+		keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-t>"] = "actions.select_tab",
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = "actions.close",
+		["<C-r>"] = "actions.refresh",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["`"] = "actions.cd",
+		["~"] = "actions.tcd",
+		["gs"] = "actions.change_sort",
+		["gx"] = "actions.open_external",
+		["g."] = "actions.toggle_hidden",
+		["g\\"] = "actions.toggle_trash",
+		},
+	  use_default_keymaps = false,
+	},
+
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
