753e6414b8f81268b52950b3f5fb5f3a98d6930a
René Weselowski <rweselowski@anexia-it.com>
2024-03-29 10:23:06 +0100

    make vim-options a plugin
    now we do not have to include it explicitly in the init.lua

--- lua/custom/plugins/vim-options.lua
+++ lua/custom/plugins/vim-options.lua
@@ -1,12 +1,12 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.background = "light"
-
+vim.api.nvim_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', { noremap = true })
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.swapfile = false
 
+-- vim.g.mapleader = ' '
 -- Navigate vim panes better
 -- vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 -- vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
@@ -15,3 +15,7 @@ vim.opt.swapfile = false
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+
+vim.g.background = 'light'
+
+return {}
