b5925630eb6f62cf93548412c41087444505bd37
René Weselowski <rweselowski@anexia-it.com>
2024-04-02 16:05:16 +0200

    add autosave

---
+++ lua/custom/plugins/autosave.lua
@@ -0,0 +1,9 @@
+return {
+  'okuuva/auto-save.nvim',
+  cmd = 'ASToggle',
+  event = { 'InsertLeave', 'TextChanged' },
+  opts = {
+    -- your config goes here
+    -- or just leave it empty :)
+  },
+}
