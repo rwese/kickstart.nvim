d1e0b3c2ca909871a6429f2d45da2768fbab05d9
René Weselowski <rweselowski@anexia-it.com>
2024-04-12 09:53:22 +0200

    plugin: autosave disable for oil buffers

--- lua/custom/plugins/autosave.lua
+++ lua/custom/plugins/autosave.lua
@@ -1,9 +1,23 @@
+vim.keymap.set("n", "<leader>n", ":ASToggle<CR>", {desc="Toggle autosave"})
+
 return {
   'okuuva/auto-save.nvim',
   cmd = 'ASToggle',
+  lazy = false,
   event = { 'InsertLeave', 'TextChanged' },
   opts = {
-    -- your config goes here
-    -- or just leave it empty :)
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+
+      -- vim.notify("filetype: " .. fn.getbufvar(buf, "&filetype"))
+      if
+        fn.getbufvar(buf, "&modifiable") == 1 and
+        utils.not_in(fn.getbufvar(buf, "&filetype"), {'autocmd', 'notify', 'oil'}) then
+        return true
+      end
+      return false
+    end,
+    write_all_buffers = false,
   },
 }
