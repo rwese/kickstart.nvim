d5945bd928f2b0ad5605737ef270b2d2d1f76839
René Weselowski <rweselowski@anexia-it.com>
2024-04-19 15:31:38 +0200

    move git-signs settings to custom

---
+++ lua/custom/plugins/git-signs.lua
@@ -0,0 +1,20 @@
+return {
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+    config = function()
+      require('gitsigns').setup()
+
+      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>')
+      vim.keymap.set('n', '<leader>gb', ':Gitsigns blame_line<CR>')
+    end,
+  },
+}
