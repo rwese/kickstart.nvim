3039064ccaf07eb2a18892e3723d14640df3e5e8
René Weselowski <rene.weselowski@gmail.com>
2024-04-20 14:29:48 +0200

    adds git keybinds

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -1,8 +1,6 @@
+vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>', { desc = 'Git commit' })
+vim.keymap.set('n', '<leader>gg', '<cmd>Git<CR>', { desc = 'Git status' })
+
 return {
   'tpope/vim-fugitive',
-  conf = function()
-    local fugitive = require 'fugitive'
-
-    fugitive.setup {}
-  end,
 }
