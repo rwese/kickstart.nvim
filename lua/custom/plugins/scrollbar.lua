8f88ea695a0c63d945dce78efc829f2629ad88d7
René Weselowski <rene.weselowski@gmail.com>
2024-04-26 07:43:11 +0200

    add some ui, scrollbar, highlights

--- lua/custom/plugins/scrollbar.lua
+++ lua/custom/plugins/scrollbar.lua
@@ -1,10 +1,26 @@
 return {
   'petertriho/nvim-scrollbar',
   config = function()
-    require('scrollbar').setup()
+    require('scrollbar').setup {
+      handle = {
+        text = '  ',
+      },
+      marks = {
+        Cursor = { text = '💠' },
+        Search = { text = { '--', '==' }, color = '#FFFF00' },
+        Error = { text = { '--', '==' } },
+        Warn = { text = { '--', '==' } },
+        Info = { text = { '--', '==' } },
+        Hint = { text = { '--', '==' } },
+        Misc = { text = { '--', '==' } },
+      },
+    }
     require('gitsigns').setup()
     require('scrollbar.handlers.gitsigns').setup()
     require('scrollbar.handlers.search').setup()
   end,
-  dependencies = { 'neoclide/coc.nvim', 'kevinhwang91/nvim-hlslens' },
+  dependencies = {
+    -- 'neoclide/coc.nvim',
+    'kevinhwang91/nvim-hlslens',
+  },
 }
